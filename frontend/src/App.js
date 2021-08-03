e0d4772c42bbc8a7b093313e445f4c43351b19a0
Deekshith Bommisetty <writingtodeek@gmail.com>
2021-08-03 19:27:28 -0400

    rating and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import react, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Header />
       <main>
         <Container className='py-3'>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </Fragment>
+    </Router>
   );
 };
 
