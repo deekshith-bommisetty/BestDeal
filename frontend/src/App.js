7f7ccbe6f02daa00e99997025fabc659e0b332fe
Deekshith Bommisetty <writingtodeek@gmail.com>
2021-08-03 18:28:52 -0400

    header-footer-homeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import react, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
-  return <Fragment>My APP</Fragment>;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default App;
