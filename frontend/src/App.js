078c69bef307908754f6be2d5afbc6eab6869369
Deekshith Bommisetty <writingtodeek@gmail.com>
2021-08-06 13:00:42 -0400

    redux setup-homescreen-productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,24 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Container className='py-3'>
-          <Route exact path='/' component={HomeScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <main>
+          <Container className='py-3'>
+            <Route exact path='/' component={HomeScreen} />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </Provider>
   );
 };
 
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import { productListReducer, productDetailsReducer } from './productReducers';
+
+export default combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
