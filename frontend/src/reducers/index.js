2b75ddcc349cbd9f6d57da19b87307acb3d7f540
Deekshith Bommisetty <writingtodeek@gmail.com>
2021-08-08 09:55:59 -0400

    signin-signup-auth

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
 import { cartReducer } from './cartReducers';
 import { productListReducer, productDetailsReducer } from './productReducers';
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from './userReducers';
 
 export default combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
